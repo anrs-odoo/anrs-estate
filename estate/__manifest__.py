02c0806587bc7c944acba9700bda5f19cc9427b1
ANRS <anrs@odoo.com>
2023-09-27 10:08:21 +0200

    logo part 5

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,7 +11,6 @@
     'auto_install': False,
     'data': [
         'security/ir.model.access.csv',
-        'static/description/icon_estate.png',
         'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ]
