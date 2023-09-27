3b091de28a8b9598c16a7b61758d775751445986
ANRS <anrs@odoo.com>
2023-09-27 10:35:13 +0200

    plein de trucs

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 class Estate(models.Model): 
 	_name = "real_estate"
@@ -7,5 +8,6 @@ class Estate(models.Model):
 	name = fields.Char("Name", default="Unknown")
 	last_seen = fields.Datetime("Last Seen", default= lambda self: fields.Datetime.now())
 	bedrooms = fields.Integer("Number of bedrooms", default=2)
-	selling_price = fields.Float("Selling Price", readonly=True)
-	availability_date = fields.Datetime("Availability date")
+	selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+	availability_date = fields.Date("Availability date", default=lambda self: fields.datetime.today() + relativedelta(months=3),copy=False)
+	active = fields.Boolean(default=True)
