edb7251ae8d179ce5514bb542ca34128321abb7c
ANRS <anrs@odoo.com>
2023-09-27 10:22:59 +0200

    add new fields

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -4,4 +4,8 @@ class Estate(models.Model):
 	_name = "real_estate"
 	_description = "Estate Management"
 
-	name = fields.Char()
+	name = fields.Char("Name", default="Unknown")
+	last_seen = fields.Datetime("Last Seen", default= lambda self: fields.Datetime.now())
+	bedrooms = fields.Integer("Number of bedrooms", default=2)
+	selling_price = fields.Monetary("Selling Price", readonly=True)
+	availability_date = fields.Datetime("Availability date")
