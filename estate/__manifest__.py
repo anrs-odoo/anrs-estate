d0f49e0b99d30aeb1628bbf072fa2f9e91281ceb
ANRS <anrs@odoo.com>
2023-09-27 09:43:34 +0200

    change manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -11,7 +11,7 @@
     'auto_install': False,
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
         'views/estate_menus.xml'
     ]
 }
